6ed33a14dafcfa19d30119e127cb9fefea44ca44
Aroosh Imran <arooshimran555@gmail.com>
2025-03-23 14:59:34 +0500

    Initial commit

---
+++ questions.js
@@ -0,0 +1,35 @@
+export const questions = [
+    {
+        id: 1,
+        question: 'Aided by his 6-foot-4-inch height, which U.S. president won an estimated 300 wrestling matches?',
+        options: ['John F.Kennedy', 'Gerald Ford', 'Abaraham Lincoln', 'Franklin D. Roosevelt'],
+        answer: 'Abaraham Lincoln',
+    },
+    {
+        id: 2,
+        question: 'Which scientist was known for eating exotic animals whenever he could, including giant tortoises of the Galapagos Islands?',
+        options: ['Galileo', 'Charles Darwin','Louis Pasteur','Isaac Newton'],
+        answer: 'Charles Darwin',
+    },
+    {
+        id: 3,
+        question: 'Which author survived a train wreck, helping to rescue others from the wreckage before retrieving an early manuscript of Our Mutual Friend?',
+        options: ['Charles Dickens', 'Herman Melville','Ralph Waldo Emerson','Walt Whitman'],
+        answer: 'Charles Dickens',
+    },
+    {
+        id: 4,
+        question: 'Which famous criminal preferred the nickname “Snorky,” a term meaning snazzy, to his more common moniker “Scarface”?',
+        options: ['Lucky Luciano','Al Capone','Bugsy Siegel','John Dillinger'],
+        answer: 'Al Capone',
+    },
+    {
+        id: 5,
+        question:'Who was bullied by schoolmates for, among other things, speaking French with a thick Corsican accent?',
+        options: ['Napoléon Bonaparte','Joan of Arc','Alexandre Dumas','Marie-Antoinette'],
+        answer: 'Napoléon Bonaparte',
+    },
+];
+
+
+
